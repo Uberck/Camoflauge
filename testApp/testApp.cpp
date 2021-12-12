ad7fef78d4084189d972579303958f2af26112fc
Christos Kokkalis <Uberck@gmail.com>
2021-12-12 02:51:03 -0500

    Cleanup v2

--- testApp/testApp.cpp
+++ testApp/testApp.cpp
@@ -5,7 +5,6 @@
 #include <string>
 using namespace std;
 
-
 int main()
 {
     string strPath = "c:\\bin\\helper.ps1";
@@ -14,17 +13,16 @@ int main()
            //The function returns –1 if the named file does not exist or does not have the given mode
     if (_access(strPath.c_str(), 0) == 0)
     {
-
        // system("start powershell.exe Set-ExecutionPolicy RemoteSigned \n");
         system("start notepad.exe \n");
-        system("start powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; c:\\bin\\helper.ps1");
+        system("start powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; c:\\bin\\helper.ps1 \n");
         system("cls");
         //system("pause"); // Pause for debugging
     }
     else
     {
         system("cls");
-        cout << "Cannot find script, is the .ps1 file in the 'bin' folder in c: drive? \n";
+        cout << "Cannot find script, is the helper.ps1 file in the 'bin' folder in c: drive? \n";
         cout << "\n";
         system("pause");
     }
